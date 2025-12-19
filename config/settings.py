6e14a868d78c68c1f22a7b7b1ac8d289f6d035ad
solo3lihamed <fps60y@gmail.com>
2025-12-20 00:36:59 +0200

    22

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-x(ymao68+5le0d&s3kk#cd3nhhoods&=#ua*-&1-7sy+i@l!0f
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
